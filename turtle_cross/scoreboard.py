adcc24f341b6f6aadf1d7f75477d03088656cc6a
Yash Shah <yashshah518@gmail.com>
2021-03-20 13:16:46 +0530

    added turtle crossing game

---
+++ turtle_cross/scoreboard.py
@@ -0,0 +1,23 @@
+from turtle import Turtle
+FONT = ("Courier", 24, "normal")
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.hideturtle()
+        self.penup()
+        self.level=1
+        self.goto(-280,250)
+        self.update_scoreboard()
+    
+    def increase_level(self):
+        self.level+=1
+    
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Level: {self.level}",align="left",font=FONT)
+    
+    def game_over(self):
+        self.goto(0,0)
+        self.write("Game Over",align="center",font=FONT)
